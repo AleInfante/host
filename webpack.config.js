5ea1c99f933dfac76890c63a359ffdf27ea5f8c0
AlexaInfante <52977709+AleInfante@users.noreply.github.com>
2024-04-04 07:18:24 -0500

    Host Microfrontend first commit

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const Dotenv = require('dotenv-webpack')
+const deps = require('./package.json').dependencies
+module.exports = (_, argv) => ({
+  output: {
+    publicPath: 'https://picker-app-host.onrender.com/',
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
+  },
+
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: 'javascript/auto',
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'host',
+      filename: 'remoteEntry.js',
+      remotes: {
+        ColorPicker:
+          'mf_colorpicker@https://main--legendary-strudel-f2c6f5.netlify.app/remoteEntry.js',
+        ColorList:
+          'mf_colorlist@https://main--tiny-cobbler-abc327.netlify.app/remoteEntry.js',
+      },
+      exposes: {},
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: deps['react-dom'],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+    }),
+    new Dotenv(),
+  ],
+})
